3dfff1444e23674420d865fb18e0643fc97aac7d
Christopher Perault <cperault92@gmail.com>
2021-02-06 13:02:28 -0600

    Initial Commit

---
+++ BodyMassIndex/BodyMassIndexApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyMassIndexApp.swift
+//  BodyMassIndex
+//
+//  Created by Christopher Perault on 2/6/21.
+//
+
+import SwiftUI
+
+@main
+struct BodyMassIndexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
